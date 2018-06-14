7735c7d7a95a972168b6c80c6834f22212f19182
Mangesh Rananavare <rmangesh1@gmail.com>
2018-06-14 18:48:24 +0200

    Initial commit

---
+++ src/main/java/Parser.java
@@ -0,0 +1,13 @@
+import net.sf.json.JSONObject;
+
+/**
+ *
+ */
+public class Parser {
+
+    public JSONObject parse(JSONObject before, JSONObject after) {
+        // TODO Implement this
+        return null;
+    }
+
+}
---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+
+/**
+ *
+ */
+public class ParserTest {
+
+    Parser parser = new Parser();
+
+    @Before
+    public void setup() {
+        // TODO Load in test data from before.json and after.json
+    }
+
+    // TODO Define tests here
+
+}
